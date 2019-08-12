aaedfdd1b0760af3d956a4b6313338d8e450af8c
Gabriel Ripardo <gts.senna@gmail.com>
2019-08-12 14:51:48 -0300

    dump para criação de produtos

---
+++ produtos_auto.sql
@@ -0,0 +1,6 @@
+SELECT * FROM crud_app.products;
+
+insert into products (nome, description, preco) Values('Notebook Gamer','Marca Dell, 2TB HD, 8GB Ram, Intel i7','1800');
+insert into products (nome, description, preco) Values('Teclado Mecânico', 'Corsair RGB','350');
+insert into products (nome, description, preco) Values('Monitor Ultrawide','UltraWide 35','750');
+insert into products (nome, description, preco) Values('Mouse Gamer','Marca Multilaser; led de 5 cores','150');
